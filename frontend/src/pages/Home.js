05a8068f0af25fc2afed2de625effffebe483fe4
astonetuhame <astonetuhame@gmail.com>
2022-06-26 21:43:08 +0300

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+
+            if(response.ok)
+            {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+
+
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }
