406eb4ef0375074fb6b81ab7d72c816cfd513e0c
Astone Tuhame <taentastone@gmail.com>
2022-07-06 10:31:08 +0300

    Styling

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,8 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutContext'
 
+//date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const WorkoutDetails = ({workout}) => {
 
     const { dispatch }   = useWorkoutsContext()
@@ -24,8 +27,8 @@ return (
         <h4>{workout.title}</h4>
         <p><strong>Load(kg): </strong>{workout.load}</p>
         <p><strong>Reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <span onClick={handleClick}>delete</span>
+        <p>{formatDistanceToNow( new Date(workout.createdAt), {addSuffix: true}) }</p>
+        <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
     </div>
 )
 }
