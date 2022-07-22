96ddab086808ceff5313686b7c1c77e0c95d6b71
Astone Tuhame <taentastone@gmail.com>
2022-07-22 13:08:28 +0300

    User routes, controllers and model

---
+++ backend/routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+// controller function
+const {signupUser, loginUser} =  require('../controllers/userController')
+
+const router = express.Router()
+
+// login route
+router.post('/login', loginUser)
+
+
+// signup route
+router.post('/signup', signupUser)
+
+
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 
 // express app
@@ -18,6 +19,7 @@ next()
 
 //routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
