ff060b5badabb08ef45b3051e140013c9ea5d0da
Astone Tuhame <taentastone@gmail.com>
2022-07-26 14:40:32 +0300

    Login

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -46,4 +46,30 @@ userSchema.statics.signup = async function (email, password){
     return user
 }
 
+// static login method
+userSchema.statics.login = async function(email, password){
+     // validation
+     if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+
+    if(!user) {
+        throw Error('Incorrect Email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect password')
+    }
+
+    return user
+
+}
+
+
+
+
 module.exports = mongoose.model('User', userSchema)
