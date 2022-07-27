515dfc502014e09e9b08c5006e650f6d012eeb1f
Astone Tuhame <taentastone@gmail.com>
2022-07-27 13:35:38 +0300

    React auth context

--- frontend/src/hooks/useWorkoutContext.js
+++ frontend/src/hooks/useWorkoutContext.js
@@ -5,7 +5,7 @@ export const useWorkoutsContext = () => {
     const context = useContext(WorkoutContext)
 
     if(!context) {
-        throw Error('useWorkoutsCntext must be used inside an WorkoutsContextProvider')
+        throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
     }
 
     return context
