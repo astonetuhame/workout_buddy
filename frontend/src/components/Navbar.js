03d71d4a1ce757ba438648ea88c61d884bd42c30
astonetuhame <astonetuhame@gmail.com>
2022-08-02 13:48:39 +0300

    Login state

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
 import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const Navbar = () => {
 
     const {logout} = useLogout()
+    const {user} = useAuthContext()
 
     const handleClick = () => {
         logout()
@@ -15,13 +17,16 @@ const Navbar = () => {
                 <h1>Workout Buddy</h1>
             </Link>
             <nav>
+                { user && (
                 <div>
+                    <span>{user.email}</span>
                     <button onClick={handleClick}>Log out</button>
-                </div>
+                </div> )}
+                { !user && (
                 <div>
                  <Link to="/login">Login</Link>   
                  <Link to="/signup">Signup</Link>   
-                </div>
+                </div> )}
             </nav>
             </div>
         </header>
